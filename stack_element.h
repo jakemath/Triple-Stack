effd3d8d5db8479de5fbfcaabf57b95985626e91
Jake Mathai <jakemathai5@gmail.com>
2021-04-15 00:45:57 -0700

    Update stack_element.h

--- stack_element.h
+++ stack_element.h
@@ -16,35 +16,33 @@ using namespace std;
 const unsigned short n = 9; // Array size
 
 unsigned short occupied = 0;   // Number of occuppied array elements
-
 unsigned short s1_top = 0;   // Indexes of top element of each stack (changes with push/pop operations)
 unsigned short s2_top = 1;
 unsigned short s3_top = 2;
 
 template <class T>
-class stack_element
-{
-public:
-    stack_element<T>() : data(0), index(-1), stack_number(0), exist(0) {}
-    stack_element<T>(T input, unsigned short idx, unsigned short stack) : data (input), index (idx), stack_number (stack), exist (1) {}
-    ~stack_element<T>();
-    
-    bool exists() { return exist; }    // Boolean indicating if array element contains valid data
-    void set_exists (bool e) { exist = e; }
-    
-    T get_data() { return data; }
-    void set_data (T input) { data = input; }
-    
-    short get_index() { return index; }
-    void set_index (unsigned short i) { index = i; }
-    
-    void operator = (stack_element* n);
-    
-private:    
-    T data;     // Stack data type
-    unsigned short index;    // Array position
-    unsigned short stack_number; // Number between 1 and 3 specifying which stack the element is a member of
-    bool exist;
+class stack_element {
+    public:
+        stack_element<T>() : data(0), index(-1), stack_number(0), exist(0) {}
+        stack_element<T>(T input, unsigned short idx, unsigned short stack) : 
+            data(input), 
+            index(idx), 
+            stack_number(stack), 
+            exist(1) {}
+        ~stack_element<T>();
+
+        bool exists() { return exist; }    // Boolean indicating if array element contains valid data
+        void set_exists (bool e) { exist = e; }
+        T get_data() { return data; }
+        void set_data (T input) { data = input; }
+        short get_index() { return index; }
+        void set_index (unsigned short i) { index = i; }
+        void operator = (stack_element* n);
+    private:    
+        T data;     // Stack data type
+        unsigned short index;    // Array position
+        unsigned short stack_number; // Number between 1 and 3 specifying which stack the element is a member of
+        bool exist;
 };
 
 template <class T>
